3a8d9ab3a0181118377ff89ab3d1c67ffc0e6252
fenixjin <fenixjin@gmail.com>
2015-03-08 13:54:55 +0800

    completing new task to 40%

--- trail1-1.c
+++ trail1-1.c
@@ -15,4 +15,22 @@
 	printf(fmt,  ##args);\
 	printf(":%s\n", strerror(errno))
 
-inline int cp_rw(int srcfd, int dstfd)
+inline int cp_rw(int srcfd, int dstfd);
+inline int cp_rw(int srcfd, int dstfd, char *buf, int len)
+{
+	int nread;
+	while （nread = read(srcfd, buf len) > 0）
+	{
+		if (write(dstfd, buf, nread) != nread)
+		{
+			error("write error");
+			return -1;
+		}
+		if (nread == -1)
+		{
+			error ("read error");
+			return -1;
+		}
+		return 0;
+	}
+}
