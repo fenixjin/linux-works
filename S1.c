1b58816839c3add8c40be5acc42bb231bdc4ae0e
fenixjin <fenixjin@gmail.com>
2015-03-16 15:12:20 +0800

    completing task2

--- S1.c
+++ S1.c
@@ -23,3 +23,19 @@ gint progress_timeout(gpointer data)
 	gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_bar), text);
 	return TRUE;
 }
+
+int main (int argc, char *argv[])
+{
+	int timer;
+	gpoint data;
+	gtk_init(&argc, &argv);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Swindow1");
+	gtk_widget_set_usize(window, 200,200);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
+	gtk_widget_show(window);
+	gtk_signal_connect(GTK_OBJECT(window), "destroy", GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
+	table = gtk_table_new(5, 11, TRUE);
+	
+
+}
