d3a09ddc59bfe761d2a97d5f158882eb91f9b22b
fenixjin <fenixjin@gmail.com>
2015-03-18 10:24:16 +0800

    finishing task 2

--- S1.c
+++ S1.c
@@ -1,4 +1,4 @@
-#include <gtk/gtk.h>
+#include <gtk-2.0/gtk/gtk.h>
 #include <string.h>
 
 GtkWidget *window;
@@ -12,14 +12,14 @@ gint progress_timeout(gpointer data)
 	int v;
 	char text[20] = "0%";
 
-	value = gtk_progress_bar_get_fraction(GTK_PROGRESSBAR(progress_bar)) + 0.01;
+	value = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(progress_bar)) + 0.01;
 	if(value > 1.0)
 		value = 0.0;
 	v = (int)(value * 100);
 	strcpy(text, " ");
 	sprintf(text, "%d", v);
 	strcat(text, "% ");
-	gtk_progress_bar_set_fraction(GTK+PROGRESS_BAR(progress_bar), value);
+	gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), value);
 	gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_bar), text);
 	return TRUE;
 }
@@ -27,17 +27,19 @@ gint progress_timeout(gpointer data)
 int main (int argc, char *argv[])
 {
 	int timer;
-	gpoint data;
+	gpointer data;
 	gtk_init(&argc, &argv);
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Swindow1");
+	gtk_window_set_title(GTK_WINDOW(window), "act1");
 	gtk_widget_set_usize(window, 200,200);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 	gtk_widget_show(window);
 	gtk_signal_connect(GTK_OBJECT(window), "destroy", GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
 	table = gtk_table_new(5, 11, TRUE);
 	gtk_widget_show(table);
-	gtk_container_add(GTK_TABLE(window), progress_bar, 0, 11, 1,2);
+	gtk_container_add(GTK_CONTAINER(window), table);
+	progress_bar = gtk_progress_bar_new();
+	gtk_table_attach_defaults(GTK_TABLE(table), progress_bar, 0, 11, 1, 2);
 	gtk_widget_show(progress_bar);
 	timer - gtk_timeout_add(150, progress_timeout, data);
 
--- S3.c
+++ S3.c
@@ -1,51 +1,48 @@
+#include <gtk-2.0/gtk/gtk.h>
 #include <string.h>
+
 GtkWidget *window;
 GtkWidget *table;
-GtkWidget *label;
-GtkWidget *text;
-int i = 0;
+GtkWidget *button;
+GtkWidget *progress_bar;
 
-gint text_timeout(gpointer data)
+gint progress_timeout(gpointer data)
 {
-	char buf[20], temp[10];
+	gdouble value;
+	int v;
+	char text[20] = "0%";
 
-	sprintf(temp, "%d", i);
-	strcpy(buf, "text ");
-	strcat(buf , temp);
-	strcat(buf, "\n");
-	gtk_text_insert(GTK_TEXT(text), NULL, NULL, NULL, buf, -1);
-	i++;
+	value = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(progress_bar)) + 0.03;
+	if(value > 1.0)
+		value = 0.0;
+	v = (int)(value * 100);
+	strcpy(text, " ");
+	sprintf(text, "%d", v);
+	strcat(text, "% ");
+	gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), value);
+	gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_bar), text);
 	return TRUE;
 }
 
 int main (int argc, char *argv[])
 {
-	GtkWidget *vscrollbar;
 	int timer;
 	gpointer data;
 	gtk_init(&argc, &argv);
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_Set_title(GTK_WINDOW(window), "Swindow3");
-	gtk_widget_set_usize(window, 200, 400);
+	gtk_window_set_title(GTK_WINDOW(window), "act3");
+	gtk_widget_set_usize(window, 200,200);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
-	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 	gtk_widget_show(window);
-
 	gtk_signal_connect(GTK_OBJECT(window), "destroy", GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
-	table = gtk_table_bew(11, 10, TRUE);
-	gtk_container_add(GTK_CONTAINER(window), table);
+	table = gtk_table_new(5, 11, TRUE);
 	gtk_widget_show(table);
+	gtk_container_add(GTK_CONTAINER(window), table);
+	progress_bar = gtk_progress_bar_new();
+	gtk_table_attach_defaults(GTK_TABLE(table), progress_bar, 0, 11, 1, 2);
+	gtk_widget_show(progress_bar);
+	timer - gtk_timeout_add(150, progress_timeout, data);
 
-	label = gtk_label_new("Text")
-	gtk_table_attach_defaults(GTK_TABLE(table), label, 0, 10, 0, 1);
-	gtk_widget_show(label);
-	text = gtk_text_new(NULL, NULL);
-	gtk_widget_show(text);
-	gtk_table_attach_defaults(GTK_TABLE(table), text, 0, 9, 1, 11);
-	vscrollbar = gtk_vscrollbar_new(GTK_TEXT(text)->vadj);
-	gtk_widget_show(vscrollbar);
-	gtk_table_attach_defaults(GTK_TABLE(table), vscrollbar, 9, 10, 1, 11);
-	timer = gtk_timeout_add(1000,text_timeout, data);
 	gtk_main();
 	return 0;
 }
